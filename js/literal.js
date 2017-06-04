795afffff62cf65405798bb566f7e6fac9c77c21
Minako Nakanishi <n135216e@gmail.com>
2017-06-05 00:13:50 +0900

    jsの追加

---
+++ js/literal.js
@@ -0,0 +1,8 @@
+let number = 20;/** 整数の宣言 */
+console.log("number "+ number);
+
+const rootTemp = 20.5;/** 小数 */
+console.log("rootTemp "+rootTemp);
+
+const c = 3.0e6;/** 3.0×(10の6乗) */
+console.log("c "+ c);
