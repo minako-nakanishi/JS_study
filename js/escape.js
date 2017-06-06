4329746d7fa65e69ed875e1d983c451f144ec55b
Minako Nakanishi <n135216e@gmail.com>
2017-06-07 00:03:17 +0900

    エスケープ文字

---
+++ js/escape.js
@@ -0,0 +1,16 @@
+const dialog = 'He looked up, and said "This is a pen!",as she walked in.';
+console.log(dialog);
+
+const imperative = "Don't run!";
+console.log(imperative);
+
+const dialog2 = "Wendy looked up and said \"Don't do that!\" to him";
+console.log(dialog2);
+
+const dialog3 = 'Wendy looked up and said "Don\'t do that!" to him';
+console.log(dialog3);
+console.log(dialog2===dialog3);
+
+const dialog4 = `Wendy looked up and said "Don't do that!" to him`;
+console.log(dialog4);
+console.log(dialog4===dialog3);
