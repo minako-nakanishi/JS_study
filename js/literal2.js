5aab330146ce37dff8bc859c007a402f47dc2a2e
Minako Nakanishi <n135216e@gmail.com>
2017-06-06 23:40:34 +0900

    add literals

--- js/literal2.js
+++ js/literal2.js
@@ -3,3 +3,21 @@ console.log("minNum "+ minNum);
 
 const largestNum = Number.MAX_SAFE_INTEGER; /** 表現できる最大の整数 */
 console.log("largestNum "+largestNum);
+
+const maxNum = Number.MAX_VALUE; /** 表現できる最大の数値 */
+console.log("maxNum "+maxNum);
+
+const smallestNum = Number.MIN_SAFE_INTEGER; /** 表現できる最小の整数 */
+console.log("smallestNum "+smallestNum);
+
+const minNums = Number.MIN_VALUE; /** 表現できる最小の数値 */
+console.log("minNums "+minNums);
+
+const nInf = Number.NEGATIVE_INFINITY;
+console.log("nInf "+nInf);
+
+const inf = Number.POSITIVE_INFINITY;
+console.log("inf "+inf);
+
+const notNum = Number.NaN;
+console.log("notNum "+notNum);
