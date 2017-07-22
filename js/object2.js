fea9a07184c8b7d4e1cd1c08ea4e5a3d663f310d
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-23 00:13:07 +0900

    delete 演算子

--- js/object2.js
+++ js/object2.js
@@ -15,10 +15,25 @@ console.log(John === John2); // false
 
 const John3 = {
     name: 'John',
-    classification: {
+    classification: { /** プロパティの値がオブジェクト */
         kingdom: 'Animalia', //動物界
         phylum: 'Chordata', //脊索動物
         class: 'Mamalia', //哺乳類
         order: 'Carnivora', //食肉類
-    }
-}
+        family: 'Felidae', // ネコ科
+        subfamily: 'Felinae', //ネコ亜科
+        genus: 'Felis', // ネコ属
+        species: 'F.catus', //ネコ
+    },
+};
+console.log(John3.classification.family) // Felinae
+console.log(John3["classification"].family); //Felinae
+console.log(John3.classification["family"]); //Felinae
+console.log(John3["classification"]["family"]); //Felinae
+
+/** オブジェクトはプロパティとして関数を持つことができる */
+John3.speak = function() { return "Meow!" };
+console.log(John3.speak()); // Meow!
+
+delete John3.classification;
+console.log(John3); //{ name: 'John', speak: [Function] }
